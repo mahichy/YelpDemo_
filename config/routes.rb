f67eea96884facec83efcdd3e1dbb772179d22e5
mahi <mohiddinoshim@yahoo.com>
2021-01-18 19:08:54 +0600

    Add home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :resturents
   get 'pages/about'
   get 'pages/contact'
+
+  root 'resturents#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
