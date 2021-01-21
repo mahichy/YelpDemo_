1fe0174eff399ce1557a8c53dc3beadf1210ae45
mahi <mohiddinoshim@yahoo.com>
2021-01-21 21:38:10 +0600

    Link Reviews to resturent

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -8,14 +8,14 @@ module DeviseHelper
 
 		html = <<-HTML
 		<div class="alert alert-danger alert-dismissable">
-			<button type="button" class="close" data-dismiss="alert"> <span aria-hidden="true">&times;</span>
+			<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span>
 				<span class="sr-only">Close</span>
-				
 			</button>
 			<h4>#{sentence}</h4>
+			#{messages}
 		</div>
-			HTML
+		HTML
 
-			html.html_safe
+		html.html_safe
 	end
 end
