9c08b444f09b74315f0e88f6bd27d2f31292a812
mahi <mohiddinoshim@yahoo.com>
2021-02-06 17:40:02 +0600

    Add Fields to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_20_092614) do
+ActiveRecord::Schema.define(version: 2021_02_06_112048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "restaurants", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.string "phone"
-    t.string "website"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "image"
-  end
 
   create_table "resturents", force: :cascade do |t|
     t.string "name"
@@ -52,6 +42,9 @@ ActiveRecord::Schema.define(version: 2021_01_20_092614) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
